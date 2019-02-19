757e779e104010e7b2d6abce32e0e8e164c5c0cc
torikraju <torikraju@hotmail.com>
2019-02-19 15:01:23 +0600

    Why you should avoid returning lists in APIs

--- movie-catalog-service/src/main/java/edu/torikraju/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/edu/torikraju/moviecatalogservice/resource/MovieCatalogResource.java
@@ -2,17 +2,14 @@ package edu.torikraju.moviecatalogservice.resource;
 
 import edu.torikraju.moviecatalogservice.model.CatalogItem;
 import edu.torikraju.moviecatalogservice.model.Movie;
-import edu.torikraju.moviecatalogservice.model.Rating;
+import edu.torikraju.moviecatalogservice.model.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,23 +29,17 @@ public class MovieCatalogResource {
 
     @RequestMapping(value = "/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable String userId) {
-
-
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 3),
-                new Rating("5678", 4)
-        );
-
-        return ratings.stream().map(rating -> {
+        UserRating ratings = restTemplate.getForObject("http://127.0.0.1:8083/ratingsData/users/" + userId, UserRating.class);
+        return ratings.getRatings().stream().map(rating -> {
             Movie movie = restTemplate.getForObject("http://127.0.0.1:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        }).collect(Collectors.toList());
+    }
+}
+
 //            Movie movie = webClientBuilder.build()
 //                    .get()
 //                    .uri("http://127.0.0.1:8082/movies/" + rating.getMovieId())
 //                    .retrieve()
 //                    .bodyToMono(Movie.class)
 //                    .block();
-            return new CatalogItem(movie.getName(), "Test", rating.getRating());
-        }).collect(Collectors.toList());
-    }
-
-}
--- rating-data-service/src/main/java/edu/torikraju/ratingdataservice/resource/RatingResource.java
+++ rating-data-service/src/main/java/edu/torikraju/ratingdataservice/resource/RatingResource.java
@@ -1,9 +1,13 @@
 package edu.torikraju.ratingdataservice.resource;
 
 import edu.torikraju.ratingdataservice.model.Rating;
+import edu.torikraju.ratingdataservice.model.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsData")
@@ -12,6 +16,17 @@ public class RatingResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRatings(@PathVariable String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 3),
+                new Rating("5678", 4)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setRatings(ratings);
+        return userRating;
     }
 
 }
