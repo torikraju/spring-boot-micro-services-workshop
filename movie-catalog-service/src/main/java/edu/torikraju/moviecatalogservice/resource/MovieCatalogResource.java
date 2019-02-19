228493bb1d3813d0860ece2f48d4223a76475e8f
torikraju <torikraju@hotmail.com>
2019-02-19 14:36:25 +0600

    Using WebClient to make API calls

--- movie-catalog-service/src/main/java/edu/torikraju/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/edu/torikraju/moviecatalogservice/resource/MovieCatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -19,8 +20,15 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    private final RestTemplate restTemplate;
+
+    private final WebClient.Builder webClientBuilder;
+
     @Autowired
-    private RestTemplate restTemplate;
+    public MovieCatalogResource(WebClient.Builder webClientBuilder, RestTemplate restTemplate) {
+        this.webClientBuilder = webClientBuilder;
+        this.restTemplate = restTemplate;
+    }
 
     @RequestMapping(value = "/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable String userId) {
@@ -33,6 +41,12 @@ public class MovieCatalogResource {
 
         return ratings.stream().map(rating -> {
             Movie movie = restTemplate.getForObject("http://127.0.0.1:8082/movies/" + rating.getMovieId(), Movie.class);
+//            Movie movie = webClientBuilder.build()
+//                    .get()
+//                    .uri("http://127.0.0.1:8082/movies/" + rating.getMovieId())
+//                    .retrieve()
+//                    .bodyToMono(Movie.class)
+//                    .block();
             return new CatalogItem(movie.getName(), "Test", rating.getRating());
         }).collect(Collectors.toList());
     }
